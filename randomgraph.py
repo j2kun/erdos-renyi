5f0528e69632d70c1a578260c8195ab61ffb4d97
Jeremy Kun <jkun2@uic.edu>
2013-08-22 15:46:08 -0500

    updated code, added animations

--- randomgraph.py
+++ randomgraph.py
@@ -13,7 +13,7 @@ class Node:
 
 def randomGraph(n,p):
    vertices = [Node(i) for i in range(n)]
-   edges = [(i,j) for i in xrange(n) for j in xrange(n) if random.random() < p]
+   edges = [(i,j) for i in xrange(n) for j in xrange(i) if random.random() < p]
 
    for (i,j) in edges:
       vertices[i].neighbors.append(vertices[j])
@@ -41,6 +41,7 @@ def connectedComponents(vertices):
         components.append(componentVisited)
         cumulativeVisited |= componentVisited
 
+   assert sum(len(c) for c in components) == len(vertices)
    return components
 
 
@@ -48,29 +49,42 @@ def sizeOfLargestComponent(vertices):
    return max(len(c) for c in connectedComponents(vertices))
 
 
-def graphLargestComponentSize(theRange):
+def graphLargestComponentSize(n, theRange):
    return [(p, sizeOfLargestComponent(randomGraph(n, p))) for p in theRange]
 
 
-def plot(data):
-   xs = [p[0] for p in data]
-   ys = [p[1] for p in data]
-   plt.plot(xs, ys)
-   plt.show()
-
 def movingAverage(a, n=3):
-    ret = numpy.cumsum(a, dtype=float)
-    return (ret[n - 1:] - ret[:1 - n]) / n
+    window = numpy.ones(n) / float(n)
+    return numpy.convolve(a, window, 'same')
+
+
+def plot(numVertices, xstart, xend, xpts, filename, windowSize=5):
+   plt.clf() # clear figure
+
+   xs = linspace(xstart, xend, num=xpts)
+   data = graphLargestComponentSize(numVertices, xs)
+
+   ys = [p[1] for p in data]
+   movingavg = movingAverage(ys, windowSize)
+   newxs = linspace(xstart, xend, num=len(movingavg))
+
+   plt.plot(xs, ys, 'b-', newxs, movingavg, 'r-', lw=2)
+   plt.xlabel("p")
+   plt.ylabel("Size of largest component")
+   plt.ylim(ymax=numVertices)
+   plt.xlim((xs[0], xs[-1]))
+   plt.title("Phase Transition for Connectivity of a Random Graph")
+   plt.savefig(filename)
 
 
 if __name__ == "__main__":
-   n = 1000
-   #data = graphLargestComponentSize(linspace(0, 0.5, num=1000))
-   data = graphLargestComponentSize(linspace(0, 0.005, num=1000))
+   plot(50, xstart=0, xend=0.5, xpts=1000, filename="zoomedout-50-1000.png", windowSize=20)
+   plot(50, xstart=0, xend=0.15, xpts=1000, filename="zoomedin-50-1000.png", windowSize=20)
+   plot(100, xstart=0, xend=0.15, xpts=1000, filename="zoomedin-100-1000.png", windowSize=20)
+   plot(500, xstart=0, xend=0.01, xpts=1000, filename="zoomedin-500-1000.png", windowSize=20)
 
-   #plot(data)
+   for i,n in enumerate(range(20, 500, 20), start=1):
+      plot(n, xstart=1.0/n, xend=5.0/n, xpts=500, filename="animation/%02d.png" % i, windowSize=20)
 
-   ys = [p[1] for p in data]
-   movingavg = movingAverage(ys, n=50)
-   plt.plot(linspace(0,.02, num=len(movingavg)), movingavg, '-')
-   plt.show()
+
+
